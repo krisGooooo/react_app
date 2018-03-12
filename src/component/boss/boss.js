35636e9d79859c1aca4c5622610649df0a5792c8
krisGooooo <shenya123@hotmail.com>
2018-03-12 23:27:00 +0800

    boss list realize
    boss list realize

--- src/component/boss/boss.js
+++ src/component/boss/boss.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import axios from 'axios'
+import {connect} from 'react-redux'
+import {Card,WhiteSpace,WingBlank} from 'antd-mobile'
+import {getUserList} from '../../redux/chatuser.redux'
+
+@connect(
+    state=>state.chatuser,
+    {getUserList}
+)
 class Boss extends React.Component{
 
     constructor(props){
@@ -9,17 +17,33 @@ class Boss extends React.Component{
         }
     }
     componentDidMount(){
-        axios.get('/user/list?type=genius')
-        .then(res=>{
-            if(res.data.code===0){
-                this.setState({data:res.data.data})
-            }
-        })
+        this.props.getUserList('genius')
     }
 
     render(){
-        console.log(this.state)
-        return null
+        const Header = Card.Header
+        const Body = Card.Body
+        return (
+            <WingBlank>
+                <WhiteSpace></WhiteSpace>
+                {this.props.userlist.map(v=>(
+
+                    v.avatar?<Card key={v._id}>
+                        <Header
+                            title={v.user}
+                            thumb={require(`../img/${v.avatar}.png`)}
+                            extra={<span>{v.title}</span>}
+                        ></Header>
+                        <Body>
+                            {v.desc.split('\n').map(v=>(
+                                <div key={v}>{v}</div>
+                            )
+                            )}
+                        </Body>
+                    </Card>:null
+                ))}
+            </WingBlank>
+        )
     }
 }
 
