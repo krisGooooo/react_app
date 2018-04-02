2b8df7cd7782d3ff6346b7591d662a6e7018da46
krisGooooo <shenya123@hotmail.com>
2018-04-02 22:43:24 +0800

    antd-motion realize

--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -4,6 +4,7 @@ import {List,InputItem,NavBar,Icon,Grid} from 'antd-mobile'
 import {connect} from 'react-redux'
 import {getMsgList,sendMsg,recvMsg,readMsg} from '../../redux/chat.redux'
 import {getChatId} from '../../util';
+import QueueAnim from 'rc-queue-anim'
 // const socket = io('ws://localhost:9093')
 
 
@@ -66,22 +67,25 @@ class Chat extends React.Component{
                     }}>
                     {users[userid].name}
                 </NavBar>
-                {chatmsgs.map(v=>{
-                    const avatar = require(`../img/${users[v.from].avatar}.png`)
-                    return  v.from===userid?(
-                        <List key={v._id}>
-                            <Item
-                                thumb={avatar}
-                            >{v.content}</Item>
+                <QueueAnim type='left' delay={100}>
+                    {chatmsgs.map(v=>{
+                        const avatar = require(`../img/${users[v.from].avatar}.png`)
+                        return  v.from===userid?(
+                            <List key={v._id}>
+                                <Item
+                                    thumb={avatar}
+                                >{v.content}</Item>
+                            </List>
+                        ):(
+                            <List key={v._id}>
+                            <Item 
+                            extra={<img alt="头像" src={avatar}/>}
+                            className='chat-me'>{v.content}</Item>
                         </List>
-                    ):(
-                        <List key={v._id}>
-                        <Item 
-                         extra={<img alt="头像" src={avatar}/>}
-                         className='chat-me'>{v.content}</Item>
-                    </List>
-                    )
-                })}
+                        )
+                    })}
+                </QueueAnim>
+
                 <div className="stick-footer">
                     <List>
                         <InputItem
--- src/component/dashboard/dashboard.js
+++ src/component/dashboard/dashboard.js
@@ -8,6 +8,7 @@ import Genius from '../../component/genius/genius'
 import User from '../../component/user/user'
 import Msg from '../msg/msg'
 import {getMsgList,recvMsg} from '../../redux/chat.redux'
+import QueueAnim from 'rc-queue-anim'
 
 @connect(
     state=>state,
@@ -54,15 +55,16 @@ class Dashboard extends React.Component{
                 component:User
             }
        ]
+
+       const page = navList.find(v=>v.path==pathname)
         return (
             <div>
                 <NavBar className='fixd-header' mode='dard'>{navList.find(v=>v.path==pathname).title}</NavBar>
                 <div style={{marginTop:45}}>
-                    <Switch>
-                        {navList.map(v=>(
-                            <Route key={v.path} path={v.path} component={v.component}></Route>
-                        ))}
-                    </Switch>
+                    <QueueAnim type='scaleX' duration={800}>
+                            <Route key={page.path} path={page.path} component={page.component}></Route>
+
+                    </QueueAnim>
                 </div>
                 <NavLinkBar data={navList}></NavLinkBar>
             </div>
