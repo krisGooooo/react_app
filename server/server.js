85f326b99fdaaf1c6d4f8e6b6c5f15dc24f711ed
krisGooooo <shenya123@hotmail.com>
2018-04-05 22:08:24 +0800

    SSR code change

--- server/server.js
+++ server/server.js
@@ -1,14 +1,37 @@
+import {renderToString} from 'react-dom/server'
+import React from 'react'
+import {Provider} from 'react-redux'
+import csshook from 'css-modules-require-hook/preset'
+import assethook from 'asset-require-hook'
+import { createStore,applyMiddleware,compose } from 'redux'
+import thunk from 'redux-thunk'
+import { StaticRouter} from 'react-router-dom'
+import App from '../src/app'
+import reducers from '../src/reducer';
+assethook({
+    extensions:['png']
+})
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const userRouter = require('./user')
 const app = express()
 const model = require('./model')
+
 const Chat = model.getModel('chat')
 const path = require('path')
-//work with express
+//work with express 
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+// function App(){
+//     return (
+//         <div>
+//             <p>server render</p>
+//             <p>rocks</p>    
+//         </div>
+//     )
+// }
+// console.log(renderToString(<App></App>));
 
 io.on('connection',function (socket) {
     console.log('user login')
@@ -30,8 +53,22 @@ app.use(function(req,res,next){
     if(req.url.startsWith('/user/')||req.url.startsWith('/static/')){
         return next()
     }
-
-    return res.sendFile(path.resolve('build/index.html'))
+    const store = createStore(reducers,compose(
+        applyMiddleware(thunk)
+    ))
+    let context={}
+    const markup=renderToString((
+        <Provider store={store}>
+            <StaticRouter
+                location={req.url}
+                context={context}
+            >
+                <App></App>
+            </StaticRouter>
+        </Provider>
+    ))
+    const htmlRes =(<App></App>)
+    res.send(htmlRes)
 })
 app.use('/',express.static(path.resolve('build')))
 server.listen(9093,function () {
--- src/index.js
+++ src/index.js
@@ -3,18 +3,12 @@ import ReactDom from 'react-dom'
 import { createStore,applyMiddleware,compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import { BrowserRouter,Route,Switch} from 'react-router-dom'
-import Dashboard from './component/dashboard/dashboard'
-import AuthRoute from './component/authroute/authroute'
-import BossInfo from './container/bossinfo/bossinfo'
-import GeniusInfo from './container/geniusinfo/geniusinfo'
-// import { counter } from './index.redux'
+import { BrowserRouter} from 'react-router-dom'
 import reducers from './reducer'
-import Chat from './component/chat/chat'
+import App from './app'
 import './config'
 import './index.css'
-import Login from './container/login/login';
-import Register from './container/register/register'
+
 
 const store = createStore(reducers,compose(
     applyMiddleware(thunk),
@@ -25,18 +19,7 @@ ReactDom.render(
     (
         <Provider store={store}>
             <BrowserRouter>
-                <div>
-                    <AuthRoute></AuthRoute>
-                    <Switch>
-                        <Route path='/bossinfo' component={BossInfo}></Route>
-                        <Route path='/geniusinfo' component={GeniusInfo}></Route>
-                        <Route path='/login' component={Login}></Route>
-                        <Route path='/register' component={Register}></Route>
-                        <Route path='/chat/:user' component={Chat}></Route>
-
-                        <Route component={Dashboard}></Route>
-                    </Switch>
-                </div>
+                <App></App>
             </BrowserRouter>
         </Provider>
     ),document.getElementById('root')
